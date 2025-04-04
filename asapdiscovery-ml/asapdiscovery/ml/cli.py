2ab80ebaca0e1b8d618f22bcc2d9fc01e6b4f147
kaminow <benjamin.kaminow@choderalab.org>
2025-04-04 16:45:16 -0400

    Can't call other click methods apparently.

--- asapdiscovery-ml/asapdiscovery/ml/cli.py
+++ asapdiscovery-ml/asapdiscovery/ml/cli.py
@@ -301,10 +301,7 @@ def build(
         "model_tag": model_tag,
     }
 
-    t = _build_trainer(
-        trainer_kwargs, trainer_config_cache, overwrite_trainer_config_cache
-    )
-    return t
+    _build_trainer(trainer_kwargs, trainer_config_cache, overwrite_trainer_config_cache)
 
 
 # Functions for building a Trainer and subsequently training the model
@@ -327,8 +324,218 @@ def build(
 @trainer_args
 @overwrite_args
 @s3_args
-def build_and_train(*args, **kwargs):
-    t = build(*args, **kwargs)
+def build_and_train(
+    output_dir: Path | None = None,
+    save_weights: str | None = None,
+    weights_path: Path | None = None,
+    trainer_config_cache: Path | None = None,
+    optimizer_type: OptimizerType | None = None,
+    lr: float | None = None,
+    weight_decay: float | None = None,
+    momentum: float | None = None,
+    dampening: float | None = None,
+    b1: float | None = None,
+    b2: float | None = None,
+    eps: float | None = None,
+    rho: float | None = None,
+    optimizer_config_cache: Path | None = None,
+    use_wandb: bool | None = None,
+    wandb_project: str | None = None,
+    wandb_name: str | None = None,
+    extra_config: tuple[str] | None = None,
+    model_type: ModelType | None = None,
+    representation: str | None = None,
+    complex_representation: str | None = None,
+    ligand_representation: str | None = None,
+    protein_representation: str | None = None,
+    strategy: StrategyConfig | None = None,
+    strategy_layer_norm: bool | None = None,
+    pred_readout: ReadoutConfig | None = None,
+    combination: CombinationConfig | None = None,
+    comb_readout: ReadoutConfig | None = None,
+    max_comb_neg: bool | None = None,
+    max_comb_scale: float | None = None,
+    pred_substrate: float | None = None,
+    pred_km: float | None = None,
+    comb_substrate: float | None = None,
+    comb_km: float | None = None,
+    model_config_cache: Path | None = None,
+    representation_config_cache: Path | None = None,
+    complex_representation_config_cache: Path | None = None,
+    ligand_representation_config_cache: Path | None = None,
+    protein_representation_config_cache: Path | None = None,
+    model_rand_seed: int | None = None,
+    model_tag: str | None = None,
+    es_type: EarlyStoppingType | None = None,
+    es_patience: int | None = None,
+    es_n_check: int | None = None,
+    es_divergence: float | None = None,
+    es_burnin: int | None = None,
+    es_config_cache: Path | None = None,
+    exp_file: Path | None = None,
+    ds_cache: Path | None = None,
+    ds_config_cache: Path | None = None,
+    ds_split_type: DatasetSplitterType | None = None,
+    train_frac: float | None = None,
+    val_frac: float | None = None,
+    test_frac: float | None = None,
+    enforce_one: bool | None = None,
+    ds_rand_seed: int | None = None,
+    ds_split_dict: Path | None = None,
+    ds_split_config_cache: Path | None = None,
+    loss: tuple[str] = (),
+    loss_weights: tuple[float] = (),
+    eval_loss_weights: tuple[float] = (),
+    auto_init: bool | None = None,
+    start_epoch: int | None = None,
+    n_epochs: int | None = None,
+    batch_size: int | None = None,
+    target_prop: str | None = None,
+    cont: bool | None = None,
+    loss_dict: dict | None = None,
+    device: torch.device | None = None,
+    data_aug: tuple[str] = (),
+    trainer_weight_decay: float | None = None,
+    batch_norm: bool | None = None,
+    overwrite_trainer_config_cache: bool = False,
+    overwrite_optimizer_config_cache: bool = False,
+    overwrite_model_config_cache: bool = False,
+    overwrite_es_config_cache: bool = False,
+    overwrite_ds_config_cache: bool = False,
+    overwrite_ds_cache: bool = False,
+    overwrite_ds_split_config_cache: bool = False,
+    s3_path: str | None = None,
+    upload_to_s3: bool | None = None,
+):
+    # Build each dict and pass to Trainer
+    optim_config = {
+        "cache": optimizer_config_cache,
+        "overwrite_cache": overwrite_optimizer_config_cache,
+        "optimizer_type": optimizer_type,
+        "lr": lr,
+        "weight_decay": weight_decay,
+        "momentum": momentum,
+        "dampening": dampening,
+        "b1": b1,
+        "b2": b2,
+        "eps": eps,
+        "rho": rho,
+    }
+    representation = (
+        kvp_list_to_dict(representation) if representation is not None else {}
+    )
+    representation |= {"cache": representation_config_cache}
+    complex_representation = (
+        kvp_list_to_dict(complex_representation)
+        if complex_representation is not None
+        else {}
+    )
+    complex_representation |= {"cache": complex_representation_config_cache}
+    ligand_representation = (
+        kvp_list_to_dict(ligand_representation)
+        if ligand_representation is not None
+        else {}
+    )
+    ligand_representation |= {"cache": ligand_representation_config_cache}
+    protein_representation = (
+        kvp_list_to_dict(protein_representation)
+        if protein_representation is not None
+        else {}
+    )
+    protein_representation |= {"cache": protein_representation_config_cache}
+    model_config = {
+        "cache": model_config_cache,
+        "overwrite_cache": overwrite_model_config_cache,
+        "model_type": model_type,
+        "representation": representation,
+        "complex_representation": complex_representation,
+        "ligand_representation": ligand_representation,
+        "protein_representation": protein_representation,
+        "strategy": strategy,
+        "strategy_layer_norm": strategy_layer_norm,
+        "pred_readout": pred_readout,
+        "combination": combination,
+        "comb_readout": comb_readout,
+        "max_comb_neg": max_comb_neg,
+        "max_comb_scale": max_comb_scale,
+        "pred_substrate": pred_substrate,
+        "pred_km": pred_km,
+        "comb_substrate": comb_substrate,
+        "comb_km": comb_km,
+        "rand_seed": model_rand_seed,
+    }
+    es_config = {
+        "cache": es_config_cache,
+        "overwrite_cache": overwrite_es_config_cache,
+        "es_type": es_type,
+        "patience": es_patience,
+        "n_check": es_n_check,
+        "divergence": es_divergence,
+        "burnin": es_burnin,
+    }
+    ds_config = {
+        "cache": ds_config_cache,
+        "overwrite_cache": overwrite_ds_config_cache,
+        "exp_file": exp_file,
+        "is_structural": False,
+        "cache_file": ds_cache,
+        "overwrite": overwrite_ds_cache,
+    }
+
+    ds_splitter_config = {
+        "cache": ds_split_config_cache,
+        "overwrite_cache": overwrite_ds_split_config_cache,
+        "split_type": ds_split_type,
+        "grouped": model_type == ModelType.grouped,
+        "train_frac": train_frac,
+        "val_frac": val_frac,
+        "test_frac": test_frac,
+        "enforce_one": enforce_one,
+        "rand_seed": ds_rand_seed,
+        "split_dict": ds_split_dict,
+    }
+    loss_configs = [kvp_list_to_dict(loss_str) for loss_str in loss]
+    data_aug_configs = [kvp_list_to_dict(aug_str) for aug_str in data_aug]
+
+    # Parse loss_dict
+    if loss_dict:
+        loss_dict = json.loads(loss_dict.read_text())
+
+    # Gather all the configs
+    trainer_kwargs = {
+        "optimizer_config": optim_config,
+        "mtenn_model_config": model_config,
+        "es_config": es_config,
+        "ds_config": ds_config,
+        "ds_splitter_config": ds_splitter_config,
+        "loss_configs": loss_configs,
+        "loss_weights": loss_weights,
+        "eval_loss_weights": eval_loss_weights,
+        "data_aug_configs": data_aug_configs,
+        "weight_decay": trainer_weight_decay,
+        "batch_norm": batch_norm,
+        "auto_init": auto_init,
+        "start_epoch": start_epoch,
+        "n_epochs": n_epochs,
+        "batch_size": batch_size,
+        "target_prop": target_prop,
+        "cont": cont,
+        "loss_dict": loss_dict,
+        "device": device,
+        "output_dir": output_dir,
+        "save_weights": save_weights,
+        "use_wandb": use_wandb,
+        "wandb_project": wandb_project,
+        "wandb_name": wandb_name,
+        "extra_config": Trainer.parse_extra_config(extra_config),
+        "s3_path": s3_path,
+        "upload_to_s3": upload_to_s3,
+        "model_tag": model_tag,
+    }
+
+    t = _build_trainer(
+        trainer_kwargs, trainer_config_cache, overwrite_trainer_config_cache
+    )
     t.initialize()
     t.train()
 
