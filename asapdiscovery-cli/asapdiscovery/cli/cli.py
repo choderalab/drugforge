14d8fe4263a74b234e0c5668a008aa0767a35833
Alexander Matthew Payne <alexpayne1997@gmail.com>
2025-07-08 17:10:26 -0400

    update asapdiscovery.cli.cli to comment out ml cli

--- asapdiscovery-cli/asapdiscovery/cli/cli.py
+++ asapdiscovery-cli/asapdiscovery/cli/cli.py
@@ -21,17 +21,18 @@ from asapdiscovery.alchemy.cli.cli import alchemy  # noqa: F401, E402, F811
 
 cli.add_command(alchemy)
 
-try:
-    from asapdiscovery.ml.cli import ml  # noqa: F401, E402, F811
+# TODO: Re-enable ML CLI when ready
+# "ML CLI not available. Please install asapdiscovery[ml] to use the ML CLI commands."
+# The ML subpackage requires a refactor to work with Pydantic 2.
+# This has been done in PR: https://github.com/choderalab/asapdiscovery/pull/2
+# However it results in breaking changes to other parts of the repo that need to be fixed in a future release.
+# This also highlights a current issue with our cli organization, in which the cli for any package depends on being able
+# to import all of the other packages.
+# We will also address this in a future release.
 
-    cli.add_command(ml)
-except ModuleNotFoundError or ImportError:
-    from warnings import warn
+# from asapdiscovery.ml.cli import ml  # noqa: F401, E402, F811
+# cli.add_command(ml)
 
-    warn(
-        "ML CLI not available. Please install asapdiscovery[ml] to use the ML CLI commands.",
-        ImportWarning,
-    )
 
 from asapdiscovery.spectrum.cli import spectrum  # noqa: F401, E402, F811
 
