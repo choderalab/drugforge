840545b2aa01886d6dcb1625111cfb0166f91cdd
Iván Pulido <2949729+ijpulidos@users.noreply.github.com>
2025-07-08 20:13:52 -0400

    Lazy ml imports workflows (#60)
    * Lazy imports for ML scorer in workflows.
    
    * Adding warning message. Experimental ML scorer.
    
    * Moving warnings before imports. Since imports are expected to fail.
    
    * XFailing ML workflow tests separately for now
    
    * Correcting the multiple parameters
    
    * Changing default for ml_score option
    
    * Moving second ML import as lazy import
    
    * Missed one import in ligand transfer docking.
    
    * Just testing new default behavior. No ML score.
    
    * set ml_score cli default to false
    
    * add try except to ml cli
    
    * remove ml score from docking test input
    
    * make ml score default off in all docking workflows
    
    * update asapdiscovery.cli.cli to comment out ml cli
    
    ---------
    
    Co-authored-by: Alexander Matthew Payne <alexpayne1997@gmail.com>

--- asapdiscovery-cli/asapdiscovery/cli/cli.py
+++ asapdiscovery-cli/asapdiscovery/cli/cli.py
@@ -21,10 +21,18 @@ from asapdiscovery.alchemy.cli.cli import alchemy  # noqa: F401, E402, F811
 
 cli.add_command(alchemy)
 
+# TODO: Re-enable ML CLI when ready
+# "ML CLI not available. Please install asapdiscovery[ml] to use the ML CLI commands."
+# The ML subpackage requires a refactor to work with Pydantic 2.
+# This has been done in PR: https://github.com/choderalab/asapdiscovery/pull/2
+# However it results in breaking changes to other parts of the repo that need to be fixed in a future release.
+# This also highlights a current issue with our cli organization, in which the cli for any package depends on being able
+# to import all of the other packages.
+# We will also address this in a future release.
 
-from asapdiscovery.ml.cli import ml  # noqa: F401, E402, F811
+# from asapdiscovery.ml.cli import ml  # noqa: F401, E402, F811
+# cli.add_command(ml)
 
-cli.add_command(ml)
 
 from asapdiscovery.spectrum.cli import spectrum  # noqa: F401, E402, F811
 
--- asapdiscovery-workflows/asapdiscovery/workflows/docking_workflows/large_scale_docking.py
+++ asapdiscovery-workflows/asapdiscovery/workflows/docking_workflows/large_scale_docking.py
@@ -35,9 +35,7 @@ from asapdiscovery.docking.scorer import (
     ChemGauss4Scorer,
 )
 from asapdiscovery.docking.fint_scorer import FINTScorer
-from asapdiscovery.docking.ml_scorer import MLModelScorer
 from asapdiscovery.docking.meta_scorer import MetaScorer
-from asapdiscovery.ml.models import ASAPMLModelRegistry
 from asapdiscovery.modeling.protein_prep import ProteinPrepper
 from asapdiscovery.spectrum.fitness import target_has_fitness_data
 from asapdiscovery.workflows.docking_workflows.workflows import (
@@ -290,6 +288,10 @@ def large_scale_docking_workflow(inputs: LargeScaleDockingInputs):
 
     # load ml scorers
     if inputs.ml_score:
+        # TODO: We probably should have this in a separate function/callable
+        logger.warning("Using ML scorer is still experimental. Fails are expected.")
+        from asapdiscovery.docking.ml_scorer import MLModelScorer  # Lazy import
+        from asapdiscovery.ml.models import ASAPMLModelRegistry
         # check which endpoints are availabe for the target
         models = ASAPMLModelRegistry.reccomend_models_for_target(inputs.target)
         for model in models:
