3762c919d7f58f1decb32c0de818b41b9d4fbd80
Iván Pulido <2949729+ijpulidos@users.noreply.github.com>
2025-09-02 23:14:57 -0400

    Specifying gufe/openfe 1.4.x and cinnabar 0.4.1

--- drugforge-alchemy/drugforge/alchemy/schema/_util.py
+++ drugforge-alchemy/drugforge/alchemy/schema/_util.py
@@ -1,6 +1,6 @@
 from collections import Counter
 from typing import TYPE_CHECKING
-from gufe.serialization.json import JSONCodec
+from gufe.custom_typing import JSONCodec
 from alchemiscale import ScopedKey
 
 if TYPE_CHECKING:
