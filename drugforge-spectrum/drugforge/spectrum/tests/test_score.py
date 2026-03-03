22c9fa36340a9090d403c35a3dd5db3728443bb5
apayne97 <alex.payne@choderalab.org>
2026-03-03 16:27:51 -0500

    add example test for convert_to_pdbqt but it doesn't work yet

--- drugforge-spectrum/drugforge/spectrum/tests/test_score.py
+++ drugforge-spectrum/drugforge/spectrum/tests/test_score.py
@@ -93,6 +93,20 @@ def test_lig_rmsd_rdkit(protein_path, tmp_path):
     )
     assert lig_rmsd == 0
 
+class TestConvertToPDBQT:
+    def test_target_convert_to_pdbqt(self, prepped_target_path, target_prepped_vina):
+        """Test conversion of target pdb to PDBQT format for AutoDock Vina."""
+        from drugforge.spectrum.score import convert_to_pdbqt
+        print(f"Prepped target path: {prepped_target_path}")
+        vina_prepped_target_path = convert_to_pdbqt(
+            prepped_path=prepped_target_path)
+
+        # Check that the two files are the same when loaded by pybel
+        from openbabel import pybel
+        converted_mol = next(pybel.readfile("pdbqt", str(vina_prepped_target_path)))
+        test_mol = next(pybel.readfile("pdbqt", str(target_prepped_vina)))
+
+        assert converted_mol.write("pdbqt") == test_mol.write("pdbqt")
 
 def test_vina_score(prepped_target_path, prepped_ligand_path):
     """Test AutoDock Vina scoring workflow of a protein-ligand complex."""
