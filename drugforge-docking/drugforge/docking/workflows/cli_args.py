795b759a9545a33250a3e4ebb185bbd3dd80ce99
Alexander Matthew Payne <31800806+apayne97@users.noreply.github.com>
2026-03-03 21:21:17 -0500

    Add docking-only workflow (#97)
    * add docking only workflow
    
    * add reference structure to multisdf output
    
    * add logging to openeye docker
    
    * add multipose docking speed test
    
    * use local cli args
    
    * remove unnecessary cli args
    
    * add conftest files
    
    * fix cli import
    
    * add non asap target test
    
    * remove target requirement from cross-docking cli
    
    * add specific number of resulting poses to test_docking multipose docking test
    
    * add docstrings to docking testing suite
    
    * change writing reference sd tag data as an optional argument to write_results_to_multi_sdf
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    Co-authored-by: Iván Pulido <2949729+ijpulidos@users.noreply.github.com>

---
+++ drugforge-docking/drugforge/docking/workflows/cli_args.py
@@ -0,0 +1,143 @@
+import click
+
+
+def use_dask(func):
+    return click.option(
+        "--use-dask",
+        is_flag=True,
+        default=False,
+        help="Whether to use dask for parallelism.",
+    )(func)
+
+
+def dask_type(func):
+    from drugforge.data.util.dask_utils import DaskType
+
+    return click.option(
+        "--dask-type",
+        type=click.Choice(DaskType.get_values(), case_sensitive=False),
+        default=DaskType.LOCAL,
+        help="The type of dask cluster to use. Local mode is reccommended for most use cases.",
+    )(func)
+
+
+def failure_mode(func):
+    from drugforge.data.util.dask_utils import FailureMode
+
+    return click.option(
+        "--failure-mode",
+        type=click.Choice(FailureMode.get_values(), case_sensitive=False),
+        default=FailureMode.SKIP,
+        help="The failure mode for dask. Can be 'raise' or 'skip'.",
+        show_default=True,
+    )(func)
+
+
+def dask_n_workers(func):
+    return click.option(
+        "--dask-n-workers",
+        type=int,
+        default=None,
+        help="The number of workers to use with dask.",
+    )(func)
+
+
+def dask_args(func):
+    return use_dask(dask_type(dask_n_workers(failure_mode(func))))
+
+
+def ligands(func):
+    return click.option(
+        "-l",
+        "--ligands",
+        type=click.Path(resolve_path=True, exists=True, file_okay=True, dir_okay=False),
+        help="File containing ligands",
+    )(func)
+
+
+def output_dir(func):
+    return click.option(
+        "--output-dir",
+        type=click.Path(
+            resolve_path=True, exists=False, file_okay=False, dir_okay=True
+        ),
+        help="The directory to output results to.",
+        default="output",
+    )(func)
+
+
+def overwrite(func):
+    return click.option(
+        "--overwrite/--no-overwrite",
+        default=True,
+        help="Whether to overwrite the output directory if it exists.",
+    )(func)
+
+
+def input_json(func):
+    return click.option(
+        "--input-json",
+        type=click.Path(resolve_path=True, exists=True, file_okay=True, dir_okay=False),
+        help="Path to a json file containing the inputs to the workflow,  WARNING: overrides all other inputs.",
+    )(func)
+
+
+def fragalysis_dir(func):
+    return click.option(
+        "--fragalysis-dir",
+        type=click.Path(resolve_path=True, exists=True, file_okay=False, dir_okay=True),
+        help="Path to a directory containing fragments to dock.",
+    )(func)
+
+
+def structure_dir(func):
+    return click.option(
+        "--structure-dir",
+        type=click.Path(resolve_path=True, exists=True, file_okay=False, dir_okay=True),
+        help="Path to a directory containing structures.",
+    )(func)
+
+
+def pdb_file(func):
+    return click.option(
+        "--pdb-file",
+        type=click.Path(resolve_path=True, exists=True, file_okay=True, dir_okay=False),
+        help="Path to a pdb file containing a structure",
+    )(func)
+
+
+def cache_dir(func):
+    return click.option(
+        "--cache-dir",
+        type=click.Path(
+            resolve_path=True, exists=False, file_okay=False, dir_okay=True
+        ),
+        help="Path to a directory where design units are cached.",
+    )(func)
+
+
+def use_only_cache(func):
+    return click.option(
+        "--use-only-cache",
+        is_flag=True,
+        default=False,
+        help="Whether to only use the cache.",
+    )(func)
+
+
+def save_to_cache(func):
+    return click.option(
+        "--save-to-cache/--no-save-to-cache",
+        help="If the newly generated structures should be saved to the cache folder.",
+        default=True,
+    )(func)
+
+
+def loglevel(func):
+    return click.option(
+        "--loglevel",
+        type=click.Choice(["DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL"]),
+        help="The log level to use.",
+        default="INFO",
+        show_default=True,
+    )(func)
