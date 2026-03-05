19ff6978319ad3314d9d179374a06b76ea1ff614
chrisiacovella <chris.iacovella@gmail.com>
2026-03-04 16:00:31 -0800

    removing pkg_resources usage in manifold_data_validation.py

--- drugforge-data/drugforge/data/services/postera/manifold_data_validation.py
+++ drugforge-data/drugforge/data/services/postera/manifold_data_validation.py
@@ -230,9 +230,9 @@ def make_static_tags(yaml_path) -> tuple[Enum, set]:
 # OK finally we can actually make the enums
 
 # static path to the spec
-manifold_data_spec = pkg_resources.resource_filename(
-    __name__, "manifold_data_tags.yaml"
-)
+from drugforge.data.services import postera
+manifold_data_spec = get_path_string( postera) +"/manifold_data_tags.yaml"
+
 
 # make target enum and set
 (
