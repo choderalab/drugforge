61e103aea3993abc7216b92f245f5844cb752593
chrisiacovella <chris.iacovella@gmail.com>
2026-03-31 12:06:55 -0700

    reverting quantity encoder changes

--- drugforge-alchemy/drugforge/alchemy/schema/base.py
+++ drugforge-alchemy/drugforge/alchemy/schema/base.py
@@ -11,6 +11,57 @@ from pydantic import BaseModel, ConfigDict
 # The BaseModel and associated functions are copied and updated from openff.models
 
 
+class QuantityEncoder(json.JSONEncoder):
+    """
+    JSON encoder for unit-wrapped floats and NumPy arrays.
+
+    This is intended to operate on FloatQuantity and ArrayQuantity objects.
+    """
+
+    def default(self, obj):
+        if isinstance(obj, Quantity):
+
+            if isinstance(obj.magnitude, (float, int)):
+                data = obj.magnitude
+            elif isinstance(obj.magnitude, np.ndarray):
+                data = obj.magnitude.tolist()
+            else:
+                # This shouldn't ever be hit if our object models
+                # behave in ways we expect?
+                raise ValueError(
+                    f"trying to serialize unsupported type {type(obj.magnitude)}"
+                )
+            return {
+                "val": data,
+                "unit": str(obj.units),
+            }
+
+
+def custom_quantity_encoder(v):
+    """Wrap json.dump to use QuantityEncoder."""
+    return json.dumps(v, cls=QuantityEncoder)
+
+
+def json_loader(data: str) -> dict:
+    """Load JSON containing custom unit-tagged quantities."""
+    # TODO: recursively call this function for nested models
+    out: dict = json.loads(data)
+    for key, val in out.items():
+        try:
+            # Directly look for an encoded FloatQuantity/ArrayQuantity,
+            # which is itself a dict
+            v = json.loads(val)
+        except (json.JSONDecodeError, TypeError):
+            # Handles some cases of the val being a primitive type
+            continue
+        # TODO: More gracefully parse non-FloatQuantity/ArrayQuantity dicts
+        unit_ = Unit(v["unit"])
+        val = v["val"]
+        out[key] = unit_ * val
+
+    return out
+
+
 class DefaultModel(BaseModel):
     """A custom Pydantic model used by other components."""
 
