224503a92e91a7071543dc1aaf5ab48f1eae3844
kaminow <benjamin.kaminow@choderalab.org>
2026-05-27 14:33:32 -0400

    Add tests.

--- drugforge-ml/drugforge/ml/tests/test_data_aug.py
+++ drugforge-ml/drugforge/ml/tests/test_data_aug.py
@@ -5,4 +5,29 @@ from drugforge.ml.data_augmentation import PositionShuffle
 
 
 def test_shuffle_both():
-    ...
+    pos = torch.rand((5, 3))
+
+    shuff = PositionShuffle()
+    assert not torch.allclose(pos, shuff(pos))
+
+
+def test_shuffle_lig():
+    pos = torch.rand((5, 3))
+    lig_idx = torch.tensor([True, True, False, False, False])
+
+    shuff = PositionShuffle(which="lig")
+    shuff_pos = shuff(pos, lig_idx)
+
+    assert torch.allclose(pos[~lig_idx, :], shuff_pos[~lig_idx, :])
+    assert not torch.allclose(pos[lig_idx, :], shuff_pos[lig_idx, :])
+
+
+def test_shuffle_prot():
+    pos = torch.rand((5, 3))
+    lig_idx = torch.tensor([True, True, False, False, False])
+
+    shuff = PositionShuffle(which="prot")
+    shuff_pos = shuff(pos, lig_idx)
+
+    assert torch.allclose(pos[lig_idx, :], shuff_pos[lig_idx, :])
+    assert not torch.allclose(pos[~lig_idx, :], shuff_pos[~lig_idx, :])
