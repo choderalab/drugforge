f697806ad4b0d86cdb4c390bc8773e2017c6669d
kaminow <benjamin.kaminow@choderalab.org>
2026-05-29 17:35:23 -0400

    Add tests for PositionRandomize.

--- drugforge-ml/drugforge/ml/tests/test_data_aug.py
+++ drugforge-ml/drugforge/ml/tests/test_data_aug.py
@@ -1,7 +1,7 @@
 import pytest
 import torch
 
-from drugforge.ml.data_augmentation import PositionShuffle
+from drugforge.ml.data_augmentation import PositionRandomize, PositionShuffle
 
 
 @pytest.fixture()
@@ -76,6 +76,67 @@ def test_shuffled_fixed_seed(data):
     pos1 = shuff(pos)
 
     shuff = PositionShuffle(rand_seed=0)
+    pos2 = shuff(pos)
+
+    assert torch.allclose(pos1, pos2)
+
+
+def test_randomize_both(data):
+    pos = data[0]
+
+    shuff = PositionRandomize()
+    assert not torch.allclose(pos, shuff(pos))
+
+
+def test_randomize_lig(data):
+    pos, lig_idx = data
+
+    shuff = PositionRandomize(which="lig")
+    shuff_pos = shuff(pos, lig_idx)
+
+    assert torch.allclose(pos[~lig_idx, :], shuff_pos[~lig_idx, :])
+    assert not torch.allclose(pos[lig_idx, :], shuff_pos[lig_idx, :])
+
+
+def test_randomize_prot(data):
+    pos, lig_idx = data
+
+    shuff = PositionRandomize(which="prot")
+    shuff_pos = shuff(pos, lig_idx)
+
+    assert torch.allclose(pos[lig_idx, :], shuff_pos[lig_idx, :])
+    assert not torch.allclose(pos[~lig_idx, :], shuff_pos[~lig_idx, :])
+
+
+def test_randomize_lig_dict(data_dict):
+    pos = data_dict["pos"]
+    lig_idx = data_dict["lig"]
+
+    shuff = PositionRandomize(which="lig")
+    shuff_pos = shuff(data_dict)["pos"]
+
+    assert torch.allclose(pos[~lig_idx, :], shuff_pos[~lig_idx, :])
+    assert not torch.allclose(pos[lig_idx, :], shuff_pos[lig_idx, :])
+
+
+def test_randomize_prot_dict(data_dict):
+    pos = data_dict["pos"]
+    lig_idx = data_dict["lig"]
+
+    shuff = PositionRandomize(which="prot")
+    shuff_pos = shuff(data_dict)["pos"]
+
+    assert torch.allclose(pos[lig_idx, :], shuff_pos[lig_idx, :])
+    assert not torch.allclose(pos[~lig_idx, :], shuff_pos[~lig_idx, :])
+
+
+def test_randomized_fixed_seed(data):
+    pos = data[0]
+
+    shuff = PositionRandomize(rand_seed=0)
+    pos1 = shuff(pos)
+
+    shuff = PositionRandomize(rand_seed=0)
     pos2 = shuff(pos)
 
     assert torch.allclose(pos1, pos2)
