e193e1cdfef37c19ad7e54adc4afbba2212360a5
kaminow <benjamin.kaminow@choderalab.org>
2026-05-29 10:28:46 -0400

    Comment explaining new num_compounds counting.

--- drugforge-ml/drugforge/ml/analysis.py
+++ drugforge-ml/drugforge/ml/analysis.py
@@ -345,6 +345,8 @@ def calc_stats(in_fn: Path, out_fn: Path, gb_keys: str):
         preds = preds[range_idx]
         in_range = in_range[range_idx]
 
+        # When calculating stats across multiple seeds, you can have each compound id
+        #  present multiple times
         num_compounds = len(g.loc[range_idx, "compound_id"].unique())
 
         # Values and low/high bounds of 95% CIs for all stats
