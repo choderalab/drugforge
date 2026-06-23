26b12d9f65fa81162a6cae4bf68215af47ee8d2f
kaminow <benjamin.kaminow@choderalab.org>
2026-06-23 17:05:38 -0400

    More comments.

--- drugforge-ml/drugforge/ml/tests/test_data_aug.py
+++ drugforge-ml/drugforge/ml/tests/test_data_aug.py
@@ -9,6 +9,8 @@ from drugforge.ml.data_augmentation import (
 
 @pytest.fixture()
 def data():
+    # Position, ligand index, integer atomic number, and one-hot encoding for passing
+    #  directly to the data aug class
     pos = torch.rand((100, 3))
     lig = torch.randint(0, 2, (100,), dtype=bool)
     atomic_num = torch.randint(1, 101, (100,))
@@ -19,6 +21,8 @@ def data():
 
 @pytest.fixture()
 def data_dict():
+    # Position, ligand index, integer atomic number, and one-hot encoding for passing as
+    #  a dict to the data aug class
     pos = torch.rand((100, 3))
     lig = torch.randint(0, 2, (100,), dtype=bool)
     atomic_num = torch.randint(1, 101, (100,))
@@ -42,6 +46,7 @@ def test_shuffle_pos_lig(data):
     shuff = PositionShuffle(which="lig")
     shuff_pos = shuff(pos, lig_idx)
 
+    # Make sure only ligand atoms have changed
     assert torch.allclose(pos[~lig_idx], shuff_pos[~lig_idx])
     assert not torch.allclose(pos[lig_idx], shuff_pos[lig_idx])
 
@@ -53,6 +58,7 @@ def test_shuffle_pos_prot(data):
     shuff = PositionShuffle(which="prot")
     shuff_pos = shuff(pos, lig_idx)
 
+    # Make sure only protein atoms have changed
     assert torch.allclose(pos[lig_idx], shuff_pos[lig_idx])
     assert not torch.allclose(pos[~lig_idx], shuff_pos[~lig_idx])
 
@@ -65,6 +71,7 @@ def test_shuffle_pos_lig_dict(data_dict):
     shuff = PositionShuffle(which="lig")
     shuff_pos = shuff(data_dict)["pos"]
 
+    # Make sure only ligand atoms have changed
     assert torch.allclose(pos[~lig_idx], shuff_pos[~lig_idx])
     assert not torch.allclose(pos[lig_idx], shuff_pos[lig_idx])
 
@@ -77,6 +84,7 @@ def test_shuffle_pos_prot_dict(data_dict):
     shuff = PositionShuffle(which="prot")
     shuff_pos = shuff(data_dict)["pos"]
 
+    # Make sure only protein atoms have changed
     assert torch.allclose(pos[lig_idx], shuff_pos[lig_idx])
     assert not torch.allclose(pos[~lig_idx], shuff_pos[~lig_idx])
 
@@ -96,6 +104,7 @@ def test_shuffle_atomic_num_lig(data):
     shuff = PositionShuffle(which="lig")
     shuff_atomic_num = shuff(atomic_num, lig_idx)
 
+    # Make sure only ligand atoms have changed
     assert torch.allclose(atomic_num[~lig_idx], shuff_atomic_num[~lig_idx])
     assert not torch.allclose(atomic_num[lig_idx], shuff_atomic_num[lig_idx])
 
@@ -107,6 +116,7 @@ def test_shuffle_atomic_num_prot(data):
     shuff = PositionShuffle(which="prot")
     shuff_atomic_num = shuff(atomic_num, lig_idx)
 
+    # Make sure only protein atoms have changed
     assert torch.allclose(atomic_num[lig_idx], shuff_atomic_num[lig_idx])
     assert not torch.allclose(atomic_num[~lig_idx], shuff_atomic_num[~lig_idx])
 
@@ -119,6 +129,7 @@ def test_shuffle_atomic_num_lig_dict(data_dict):
     shuff = PositionShuffle(dict_key="atomic_num", which="lig")
     shuff_atomic_num = shuff(data_dict)["atomic_num"]
 
+    # Make sure only ligand atoms have changed
     assert torch.allclose(atomic_num[~lig_idx], shuff_atomic_num[~lig_idx])
     assert not torch.allclose(atomic_num[lig_idx], shuff_atomic_num[lig_idx])
 
@@ -131,6 +142,7 @@ def test_shuffle_atomic_num_prot_dict(data_dict):
     shuff = PositionShuffle(dict_key="atomic_num", which="prot")
     shuff_atomic_num = shuff(data_dict)["atomic_num"]
 
+    # Make sure only protein atoms have changed
     assert torch.allclose(atomic_num[lig_idx], shuff_atomic_num[lig_idx])
     assert not torch.allclose(atomic_num[~lig_idx], shuff_atomic_num[~lig_idx])
 
@@ -151,6 +163,7 @@ def test_shuffle_one_hot_lig(data):
     shuff = PositionShuffle(which="lig")
     shuff_one_hot = shuff(one_hot, lig_idx)
 
+    # Make sure only ligand atoms have changed
     assert torch.allclose(one_hot[~lig_idx], shuff_one_hot[~lig_idx])
     assert not torch.allclose(one_hot[lig_idx], shuff_one_hot[lig_idx])
 
@@ -163,6 +176,7 @@ def test_shuffle_one_hot_prot(data):
     shuff = PositionShuffle(which="prot")
     shuff_one_hot = shuff(one_hot, lig_idx)
 
+    # Make sure only protein atoms have changed
     assert torch.allclose(one_hot[lig_idx], shuff_one_hot[lig_idx])
     assert not torch.allclose(one_hot[~lig_idx], shuff_one_hot[~lig_idx])
 
@@ -176,6 +190,7 @@ def test_shuffle_one_hot_lig_dict(data_dict):
     shuff = PositionShuffle(dict_key="one_hot", which="lig")
     shuff_one_hot = shuff(data_dict)["one_hot"]
 
+    # Make sure only ligand atoms have changed
     assert torch.allclose(one_hot[~lig_idx], shuff_one_hot[~lig_idx])
     assert not torch.allclose(one_hot[lig_idx], shuff_one_hot[lig_idx])
 
@@ -189,6 +204,7 @@ def test_shuffle_one_hot_prot_dict(data_dict):
     shuff = PositionShuffle(dict_key="one_hot", which="prot")
     shuff_one_hot = shuff(data_dict)["one_hot"]
 
+    # Make sure only protein atoms have changed
     assert torch.allclose(one_hot[lig_idx], shuff_one_hot[lig_idx])
     assert not torch.allclose(one_hot[~lig_idx], shuff_one_hot[~lig_idx])
 
@@ -234,6 +250,7 @@ def test_randomize_pos_lig(data):
     shuff = PositionRandomize(which="lig")
     shuff_pos = shuff(pos, lig_idx)
 
+    # Make sure only ligand atoms have changed
     assert torch.allclose(pos[~lig_idx], shuff_pos[~lig_idx])
     assert not torch.allclose(pos[lig_idx], shuff_pos[lig_idx])
 
@@ -245,6 +262,7 @@ def test_randomize_pos_prot(data):
     shuff = PositionRandomize(which="prot")
     shuff_pos = shuff(pos, lig_idx)
 
+    # Make sure only protein atoms have changed
     assert torch.allclose(pos[lig_idx], shuff_pos[lig_idx])
     assert not torch.allclose(pos[~lig_idx], shuff_pos[~lig_idx])
 
@@ -257,6 +275,7 @@ def test_randomize_pos_lig_dict(data_dict):
     shuff = PositionRandomize(which="lig")
     shuff_pos = shuff(data_dict)["pos"]
 
+    # Make sure only ligand atoms have changed
     assert torch.allclose(pos[~lig_idx], shuff_pos[~lig_idx])
     assert not torch.allclose(pos[lig_idx], shuff_pos[lig_idx])
 
@@ -269,6 +288,7 @@ def test_randomize_pos_prot_dict(data_dict):
     shuff = PositionRandomize(which="prot")
     shuff_pos = shuff(data_dict)["pos"]
 
+    # Make sure only protein atoms have changed
     assert torch.allclose(pos[lig_idx], shuff_pos[lig_idx])
     assert not torch.allclose(pos[~lig_idx], shuff_pos[~lig_idx])
 
@@ -288,6 +308,7 @@ def test_randomize_int_lig(data):
     rand = PositionRandomize(which="lig", dict_key="atomic_num", data_type="int")
     rand_atomic_num = rand(atomic_num, lig_idx)
 
+    # Make sure only ligand atoms have changed
     assert torch.allclose(atomic_num[~lig_idx], rand_atomic_num[~lig_idx])
     assert not torch.allclose(atomic_num[lig_idx], rand_atomic_num[lig_idx])
 
@@ -299,6 +320,7 @@ def test_randomize_int_prot(data):
     rand = PositionRandomize(which="prot", dict_key="atomic_num", data_type="int")
     rand_atomic_num = rand(atomic_num, lig_idx)
 
+    # Make sure only protein atoms have changed
     assert torch.allclose(atomic_num[lig_idx], rand_atomic_num[lig_idx])
     assert not torch.allclose(atomic_num[~lig_idx], rand_atomic_num[~lig_idx])
 
@@ -311,6 +333,7 @@ def test_randomize_int_lig_dict(data_dict):
     rand = PositionRandomize(which="lig", dict_key="atomic_num", data_type="int")
     rand_atomic_num = rand(data_dict)["atomic_num"]
 
+    # Make sure only ligand atoms have changed
     assert torch.allclose(atomic_num[~lig_idx], rand_atomic_num[~lig_idx])
     assert not torch.allclose(atomic_num[lig_idx], rand_atomic_num[lig_idx])
 
@@ -324,6 +347,7 @@ def test_randomize_int_prot_dict(data_dict):
     rand = PositionRandomize(which="prot", dict_key="atomic_num", data_type="int")
     rand_atomic_num = rand(data_dict)["atomic_num"]
 
+    # Make sure only protein atoms have changed
     assert torch.allclose(atomic_num[lig_idx], rand_atomic_num[lig_idx])
     assert not torch.allclose(atomic_num[~lig_idx], rand_atomic_num[~lig_idx])
 
@@ -343,6 +367,7 @@ def test_randomize_one_hot_lig(data):
     rand = PositionRandomize(which="lig", dict_key="one_hot", data_type="onehot")
     rand_one_hot = rand(one_hot, lig_idx)
 
+    # Make sure only ligand atoms have changed
     assert torch.allclose(one_hot[~lig_idx], rand_one_hot[~lig_idx])
     assert not torch.allclose(one_hot[lig_idx], rand_one_hot[lig_idx])
 
@@ -354,6 +379,7 @@ def test_randomize_one_hot_prot(data):
     rand = PositionRandomize(which="prot", dict_key="one_hot", data_type="onehot")
     rand_one_hot = rand(one_hot, lig_idx)
 
+    # Make sure only protein atoms have changed
     assert torch.allclose(one_hot[lig_idx], rand_one_hot[lig_idx])
     assert not torch.allclose(one_hot[~lig_idx], rand_one_hot[~lig_idx])
 
@@ -366,6 +392,7 @@ def test_randomize_one_hot_lig_dict(data_dict):
     rand = PositionRandomize(which="lig", dict_key="one_hot", data_type="onehot")
     rand_one_hot = rand(data_dict)["one_hot"]
 
+    # Make sure only ligand atoms have changed
     assert torch.allclose(one_hot[~lig_idx], rand_one_hot[~lig_idx])
     assert not torch.allclose(one_hot[lig_idx], rand_one_hot[lig_idx])
 
@@ -379,6 +406,7 @@ def test_randomize_one_hot_prot_dict(data_dict):
     rand = PositionRandomize(which="prot", dict_key="one_hot", data_type="onehot")
     rand_one_hot = rand(data_dict)["one_hot"]
 
+    # Make sure only protein atoms have changed
     assert torch.allclose(one_hot[lig_idx], rand_one_hot[lig_idx])
     assert not torch.allclose(one_hot[~lig_idx], rand_one_hot[~lig_idx])
 
